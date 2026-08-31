2b289e4d45bca46588b84eaf449b8fc29b62adb5
Marco Borla <5006150+PeopleInside@users.noreply.github.com>
2026-08-31 13:48:46 +0200

    Update MCE_PLUGIN_VERSION to 1.3.7

--- modern-classic-editor.php
+++ modern-classic-editor.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'MCE_PLUGIN_VERSION', '1.3.6' );
+define( 'MCE_PLUGIN_VERSION', '1.3.7' );
 define( 'MCE_PLUGIN_FILE', __FILE__ );
 define( 'MCE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'MCE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
